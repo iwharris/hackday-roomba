993790bccd5982e119c9939db8f81074d8e54047
Ian Harris <ian.harris@appdirect.com>
2021-05-28 14:06:30 -0600

    Type changes and remove unused module

--- frontend/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-import { Connection, HassEntities, HassServices, HassUser } from 'home-assistant-js-websocket';
-
-export interface State {
-    entities?: HassEntities;
-    services?: HassServices;
-    user?: HassUser;
-
-    connection?: Connection;
-}
